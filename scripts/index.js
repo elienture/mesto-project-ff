7d1755a4c732c5ea8a83ffb8efbfe6d6dc443f2c
elienture <143265109+elienture@users.noreply.github.com>
2024-02-10 22:02:07 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+// константы
+const placesList = document.querySelector(".places__list");
+const card = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+// functions
 
-// @todo: Функция создания карточки
+function getCard(item, cardDelete) {
+  const cardElement = card.querySelector(".card").cloneNode(true);
+  const cardImage = card.querySelector(".card__image");
+  const cardTitle = card.querySelector(".card__title");
+
+  cardImage.src = item.link;
+  cardTitle.textContent = item.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", function () {
+    cardDelete(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+function cardDelete(item) {
+  item.remove();
+}
+
+function addCard(item) {
+  placesList.append(getCard(item, cardDelete));
+}
+
+initialCards.forEach((item) => addCard(item));
